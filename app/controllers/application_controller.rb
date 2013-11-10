eda9fb0b0b0fd94f84b19ad90433cbab70293560
Tedi Braja <bradjesz@gmail.com>
2013-11-10 20:19:51 +0700

    Fix http auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   
   def authenticate
     authenticate_or_request_with_http_basic do |username, password|
-      username == "admin  " && password == "worldisflat"
+      username == "admin" && password == "worldisflat"
     end
     # warden.custom_failure! if performed?
   end

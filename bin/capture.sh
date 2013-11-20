bd69f60014574c162ce784f6b07c558c4d1cb264
Tedi Braja <bradjesz@gmail.com>
2013-11-21 00:18:32 +0700

    rm shell script

--- bin/capture.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#avconv -i 'http://50.197.140.116:88/videostream.asf?user=admin&pwd=TheGreat123&resolution=16' -t 3600 -acodec libmp3lame ou.avi > /dev/null 2>&1 &
-
-avconv -i "$1" -t $2 -acodec libmp3lame $3 > /dev/null 2>&1 &
-echo $$

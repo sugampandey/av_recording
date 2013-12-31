98a0eb4d7a9f6568331c36f55576c77cf0af57f8
Tedi Braja <bradjesz@gmail.com>
2014-01-01 04:00:13 +0700

    update recipe

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,11 +13,18 @@ default_run_options[:pty] = true
 set :deploy_via, :remote_cache
 set :rake, 'bundle exec rake'
 set :keep_releases, 3
+before "bundle:install", "config:symlink_database_yml"
 after "deploy:restart", "deploy:cleanup"
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+namespace :config do 
+  task :symlink_database_yml do
+    run "rm -f #{release_path}/config/database.yml"
+    run "ln -sfn #{release_path}/config/database.copyme.yml #{release_path}/config/database.yml"
   end
 end
